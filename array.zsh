f1f6a2bedf01d3b5050727cafd40beee87ca3b0e
tyutaka <shigure@refy.net>
2013-04-13 19:26:59 +0900

    small changes

--- array.zsh
+++ array.zsh
@@ -5,6 +5,6 @@ function _array_toarray()
 	if [ "$LANG" != "C" ]; then
 		LANG=C _array_toarray "$1"
 	else
-		echo -n "("$(for ((i=1; i<=$#1; i+=1)) do echo -n " $((0x$(toint ${1[$i]})))"; done)")"
+		echo -n "("$(for ((i=1; i<=$#1; i+=1)) do echo -n " $((0x$(_bin_toint ${1[$i]})))"; done)")"
 	fi
 }
